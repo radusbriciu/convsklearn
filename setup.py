3b3c8091baaa0c28d5a35afa707448ef0f3f1429
radusbriciu <radusbriciu@proton.me>
2024-10-14 10:15:08 +0100

    added description

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-	name="convsklearn",
-	version="0.1",
-	packages=find_packages(),
+    name="convsklearn",
+    version="0.1.0",
+    author="RADU-STEFAN BRICIU",
+    author_email="radusbriciu@proton.me",
+    description="a proprietary package of convenience wrappers for sklearn",
+    url="https://github.com/boomelage/convsklearn",
+    packages=find_packages(),
 )
