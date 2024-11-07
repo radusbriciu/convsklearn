9cb9f7457b03037559dc37e06238a3e4792ad8af
radusbriciu <radusbriciu@proton.me>
2024-11-07 16:32:48 +0000

    testing

---
+++ convsklearn/plot_errors.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def compute_RMSE(diff):
+    if len(diff)>0:
+        return np.sqrt(np.mean(diff.values**2))
+        
+def compute_MAE(diff):
+    if len(diff)>0:
+        return np.mean(np.abs(diff.values))
+
+def plot_errors(train_data,test_data):
+    test_diff = test_data['outofsample_error']
+    train_diff = train_data['insample_error']
+    
+    test_plot = test_data[['date','spot_price','rho','v0']].copy()
+    test_plot = test_plot.reset_index().set_index('date')
+    test_plot['MAE'] = test_diff.resample('D').apply(compute_RMSE)
+    test_plot['RMSE'] = test_diff.resample('D').apply(compute_RMSE)
+    test_plot = test_plot.reset_index()
+    test_plot = test_plot.drop_duplicates(subset=['date'],keep='last').set_index('date').drop(columns='calculation_date')
+    
+    train_plot = train_data[['date','spot_price','rho','v0']].copy()
+    train_plot = train_plot.reset_index().set_index('date')
+    train_plot['MAE'] = train_diff.resample('D').apply(compute_RMSE)
+    train_plot['RMSE'] = train_diff.resample('D').apply(compute_RMSE)
+    train_plot = train_plot.reset_index()
+    train_plot = train_plot.drop_duplicates(subset=['date'],keep='last').set_index('date').drop(columns='calculation_date')
+
+    
+    trainx = pd.date_range(start=min(train_data.index),end=max(train_data.index),periods=train_plot.shape[0])
+    testx = pd.date_range(start=min(test_data.index),end=max(test_data.index),periods=test_plot.shape[0])
+
+    train_plot.index = trainx
+    test_plot.index = testx
+    
+    
+    fig,axs = plt.subplots(max(len(train_plot.columns),len(test_plot.columns)),figsize=(10,10),sharex=True)
+    for i,col in enumerate(train_plot.columns):
+        axs[i].plot(train_plot[col],color='green',label='in-sample')
+        axs[i].set_title(col.replace('_',' '))
+        axs[i].legend()
+    for i,col in enumerate(test_plot.columns):
+        axs[i].plot(test_plot[col],color='purple',label='out-of-sample')
+        axs[i].set_title(col.replace('_',' '))
+        axs[i].legend()
+    plt.show()
