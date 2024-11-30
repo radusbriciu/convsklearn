1b95ff224de2f90e963d79d26f5b2cb0750e8610
radusbriciu <radusbriciu@proton.me>
2024-11-30 18:49:03 +0000

    testing

--- convsklearn/hypertuning.py
+++ convsklearn/hypertuning.py
@@ -7,18 +7,10 @@ class hypertuning():
         self.train_y = model['train_y']
         self.model = model['model']
         self.param_grid = {
-            'regressor__activation': [
-                'identity', 
-                'logistic', 
-                'tanh', 
-                'relu'
-            ],
-            'regressor__hidden_layer_sizes' : [(10,10),(20,20),(30,30)]
-            'regressor__solver': ['lbfgs', 'sgd',],
+        
+            'regressor__hidden_layer_sizes' : [(10,10),(20,20),(30,30)],
             'regressor__alpha': [0.0001, 0.001, 0.01, 0.1],
-            
             'regressor__batch_size': ['auto', 32, 64, 128],
-
             'regressor__learning_rate': ['constant', 'invscaling', 'adaptive'],
             'regressor__learning_rate_init': [0.001, 0.01, 0.1],
             'regressor__early_stopping': [False, True],
@@ -26,17 +18,17 @@ class hypertuning():
             'regressor__max_iter': [200, 500, 1000],
             'regressor__n_iter_no_change': [10, 20],
 
-
-            'regressor__power_t': [0.5, 0.25],
-            'regressor__shuffle': [True, False],
-            'regressor__warm_start': [False, True],
-            'regressor__momentum': [0.9, 0.8, 0.7],
-            'regressor__nesterovs_momentum': [True, False],
-            'regressor__validation_fraction': [0.1, 0.15, 0.2],
-            'regressor__beta_1': [0.9, 0.85],
-            'regressor__beta_2': [0.999, 0.99],
-            'regressor__epsilon': [1e-8, 1e-6],
-            'regressor__max_fun': [15000, 20000]
+            # 'regressor__solver': ['lbfgs', 'sgd',],
+            # 'regressor__power_t': [0.5, 0.25],
+            # 'regressor__shuffle': [True, False],
+            # 'regressor__warm_start': [False, True],
+            # 'regressor__momentum': [0.9, 0.8, 0.7],
+            # 'regressor__nesterovs_momentum': [True, False],
+            # 'regressor__validation_fraction': [0.1, 0.15, 0.2],
+            # 'regressor__beta_1': [0.9, 0.85],
+            # 'regressor__beta_2': [0.999, 0.99],
+            # 'regressor__epsilon': [1e-8, 1e-6],
+            # 'regressor__max_fun': [15000, 20000]
         }
         self.search_parameters = {
             'estimator':self.model,
