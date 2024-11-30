9d50985f429ef0cd7cca4a2f6d404e0d778891c3
radusbriciu <radusbriciu@proton.me>
2024-11-30 20:13:43 +0000

    testing

---
+++ convsklearn/train_asians.py
@@ -0,0 +1,19 @@
+from convsklearn import train
+import os
+from time import time
+from model_settings import ms
+from pathlib import Path
+from df_collector import df_collector
+
+start = time()
+ms.find_root(Path())
+df_collector.root = ms.root
+raw = df_collector.cboe_spx_asians().iloc[:,1:]
+train = train()
+train.load_data(raw,verbose=False)
+train.construct(verbose=False,plot=False)
+train.fit()
+train.test_fit()
+train.save_model(dir=os.path.join(ms.root,ms.trained_models))
+runtime = time() - start
+print(f"cpu: {runtime}")
