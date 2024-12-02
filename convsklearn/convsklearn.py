6922ce1275bb9a03d22275a458fb02389a5f6bc2
radusbriciu <radusbriciu@proton.me>
2024-12-02 10:41:08 +0000

    testing

--- convsklearn/convsklearn.py
+++ convsklearn/convsklearn.py
@@ -27,7 +27,7 @@ class convsklearn:
     def __init__(
             self,
             target_name='observed_price',
-            excluded_features=['barrier_price','asian_price','observed_price','outin','updown','n_fixings'],
+            excluded_features=['barrier_price','asian_price','observed_price','outin','updown','n_fixings','barrier_cpu','asian_cpu'],
             seed=1312,
             ):
         self.seed = seed
